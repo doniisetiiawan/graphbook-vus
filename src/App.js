0d1ca6e170f12c2ac2b8efa6fec548b1e474e5d6
Doni <donisetiawan.050@gmail.com>
2019-07-15 17:05:50 +0700

    Rendering array from react state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable react/jsx-filename-extension,global-require */
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const posts = [{
+  id: 2,
+  text: 'Lorem ipsum',
+  user: {
+    avatar: require('./uploads/avatar1.png'),
+    username: 'Test User',
+  },
+},
+{
+  id: 1,
+  text: 'Lorem ipsum',
+  user: {
+    avatar: require('./uploads/avatar2.png'),
+    username: 'Test User 2',
+  },
+}];
+
+class App extends Component {
+  state = {
+    posts,
+  };
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="container">
+            <div className="feed">
+              {posts.map(post => (
+                <div key={post.id} className="post">
+                  <div className="header">
+                    <img src={post.user.avatar} alt="avatar" />
+                    <h2>{post.user.username}</h2>
+                  </div>
+                  <p className="content">
+                    {post.text}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
