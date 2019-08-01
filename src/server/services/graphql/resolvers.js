fdad9b57bcfeb57dd912ce4d19db71a08054a04f
Doni <donisetiawan.050@gmail.com>
2019-08-01 20:08:01 +0700

    Database query

--- src/server/services/graphql/resolvers.js
+++ src/server/services/graphql/resolvers.js
@@ -1,27 +1,13 @@
 import logger from '../../helpers/logger';
 
-const posts = [{
-  id: 2,
-  text: 'Lorem ipsum',
-  user: {
-    avatar: '/uploads/avatar1.png',
-    username: 'Test User',
-  },
-},
-{
-  id: 1,
-  text: 'Lorem ipsum',
-  user: {
-    avatar: '/uploads/avatar2.png',
-    username: 'Test User 2',
-  },
-}];
-
 export default function resolver() {
+  const { db } = this;
+  const { Post } = db.models;
+
   const resolvers = {
     RootQuery: {
       posts(root, args, context) {
-        return posts;
+        return Post.findAll({order: [['createdAt', 'DESC']]});
       },
     },
     RootMutation: {
