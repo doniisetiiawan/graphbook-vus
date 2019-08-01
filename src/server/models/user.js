fcd613c197f459a009cf70d04af27aaad0cff1d8
Doni <donisetiawan.050@gmail.com>
2019-08-01 20:50:14 +0700

    Model association

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,11 +1,11 @@
-'use strict';
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     avatar: DataTypes.STRING,
-    username: DataTypes.STRING
+    username: DataTypes.STRING,
   }, {});
-  User.associate = function(models) {
-    // associations can be defined here
+  User.associate = function (models) {
+    User.hasMany(models.Post);
   };
   return User;
 };
--- src/server/services/graphql/resolvers.js
+++ src/server/services/graphql/resolvers.js
@@ -5,9 +5,14 @@ export default function resolver() {
   const { Post } = db.models;
 
   const resolvers = {
+    Post: {
+      user(post, args, context) {
+        return post.getUser();
+      },
+    },
     RootQuery: {
       posts(root, args, context) {
-        return Post.findAll({order: [['createdAt', 'DESC']]});
+        return Post.findAll({ order: [['createdAt', 'DESC']] });
       },
     },
     RootMutation: {
