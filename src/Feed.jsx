3511677d62b250a0717942a301c0d29b7dbe11b5
Doni <donisetiawan.050@gmail.com>
2019-08-02 20:18:18 +0700

    Apollo mutation component

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import './App.css';
 
 const GET_POSTS = gql`{
@@ -15,6 +15,18 @@ const GET_POSTS = gql`{
     }
 }`;
 
+const ADD_POST = gql`
+    mutation addPost($post : PostInput!) {
+        addPost(post : $post) {
+            id
+            text
+            user {
+                username
+                avatar
+            }
+        }
+    }`;
+
 class Feed extends Component {
   state = {
     postContent: '',
@@ -24,60 +36,63 @@ class Feed extends Component {
     this.setState({ postContent: event.target.value });
   };
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const { postContent, posts: posts1 } = this.state;
-    const newPost = {
-      id: posts1.length + 1,
-      text: postContent,
-      user: {
-        avatar: require('./uploads/avatar1.png'),
-        username: 'Fake User',
-      },
-    };
-    this.setState(prevState => ({
-      posts: [newPost, ...prevState.posts],
-      postContent: '',
-    }));
-  };
-
   render() {
+    const self = this;
     const { postContent } = this.state;
 
     return (
-      <div className="container">
-        <div className="postForm">
-          <form onSubmit={this.handleSubmit}>
-            <textarea
-              value={postContent}
-              onChange={this.handlePostContentChange}
-              placeholder="Write your custom post!"
-            />
-            <input type="submit" value="Submit" />
-          </form>
-        </div>
-        <div className="feed">
-          <Query query={GET_POSTS}>
-            {({ loading, error, data }) => {
-              if (loading) return 'Loading...';
-              if (error) return error.message;
+      <Query query={GET_POSTS}>
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return error.message;
 
-              const { posts } = data;
-              return posts.map(post => (
-                <div key={post.id} className="post">
-                  <div className="header">
-                    <img src={post.user.avatar} alt="avatar" />
-                    <h2>{post.user.username}</h2>
+          const { posts } = data;
+
+          return (
+            <div className="container">
+              <div className="postForm">
+                <Mutation mutation={ADD_POST}>
+                  {addPost => (
+                    <form onSubmit={(e) => {
+                      e.preventDefault();
+                      addPost({
+                        variables: {
+                          post: { text: postContent },
+                        },
+                      }).then(() => {
+                        self.setState(() => ({
+                          postContent: '',
+                        }));
+                      });
+                    }}
+                    >
+                      <textarea
+                        value={postContent}
+                        onChange={self.handlePostContentChange}
+                        placeholder="Write your custom post!"
+                      />
+                      <input type="submit" value="Submit" />
+                    </form>
+                  )}
+                </Mutation>
+              </div>
+              <div className="feed">
+                {posts.map(post => (
+                  <div key={post.id} className="post">
+                    <div className="header">
+                      <img src={post.user.avatar} alt="avatar" />
+                      <h2>{post.user.username}</h2>
+                    </div>
+                    <p className="content">
+                      {post.text}
+                    </p>
                   </div>
-                  <p className="content">
-                    {post.text}
-                  </p>
-                </div>
-              ));
-            }}
-          </Query>
-        </div>
-      </div>
+                ))}
+              </div>
+            </div>
+          );
+        }}
+      </Query>
     );
   }
 }
